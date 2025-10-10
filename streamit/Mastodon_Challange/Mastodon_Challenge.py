4ac0694220010d1783123a87a46ab49f5eff2d76
Amruthraj Gudimalla <gudimalla.amruthraj29@gmail.com>
2025-10-10 15:39:26 +0200

    rr

--- streamit/Mastodon_Challange/Mastodon_Challenge.py
+++ streamit/Mastodon_Challange/Mastodon_Challenge.py
@@ -14,7 +14,7 @@ st.title("Mastodon Challenge")
 
 #####################################################################
 #Sentiment
-sentiment_df = pd.read_csv("combined_sentiment_results.csv")
+sentiment_df = pd.read_csv("./streamit/Mastodon_Challange/combined_sentiment_results.csv")
 
 #remove japanese hashtags
 
